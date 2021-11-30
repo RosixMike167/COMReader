65063e1d93b751f4cafc0ff76b191d64837e4bba
ZioPagnotta <rosi.michael167@gmail.com>
2021-11-30 16:51:05 +0100

    Aggiungere i file di progetto.

---
+++ COMReader/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace COMReader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.outputBox = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // outputBox
+            // 
+            this.outputBox.BackColor = System.Drawing.Color.DimGray;
+            this.outputBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.outputBox.DetectUrls = false;
+            this.outputBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputBox.ForeColor = System.Drawing.Color.White;
+            this.outputBox.Location = new System.Drawing.Point(12, 36);
+            this.outputBox.Name = "outputBox";
+            this.outputBox.ReadOnly = true;
+            this.outputBox.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.ForcedVertical;
+            this.outputBox.Size = new System.Drawing.Size(776, 359);
+            this.outputBox.TabIndex = 0;
+            this.outputBox.Text = "";
+            this.outputBox.TextChanged += new System.EventHandler(this.OutputBox_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(12, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Output:";
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.AutoSize = true;
+            this.outputLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel.ForeColor = System.Drawing.Color.White;
+            this.outputLabel.Location = new System.Drawing.Point(100, 12);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(45, 20);
+            this.outputLabel.TabIndex = 2;
+            this.outputLabel.Text = "none";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 401);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(133, 37);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Svuota output box";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(290, 413);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Premuto:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(394, 418);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "none";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox1.ForeColor = System.Drawing.Color.White;
+            this.checkBox1.Location = new System.Drawing.Point(697, 401);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(92, 20);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.Text = "Auto-Scroll";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(151, 401);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(133, 37);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Ferma la ricezione dati";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DimGray;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.outputBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "COM Reader v1.0";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox outputBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ COMReader/Form1.cs
@@ -0,0 +1,306 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.IO.Ports;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Automation;
+using System.Windows.Forms;
+
+namespace COMReader
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern void keybd_event(byte bVk, byte bScan, int dwFlags, int dwExtraInfo);
+
+        private static Dictionary<string, string> dictionary;
+        private Dictionary<string, string> commands;
+        private BackgroundWorker workThread;
+        private SerialPort port;
+
+        private const int KEYEVENTF_EXTENDEDKEY = 0x0001; //Key down flag
+        private const int KEYEVENTF_KEYUP = 0x0002; //Key up flag
+        private const int VK_RCONTROL = 0xA3; //Right Control key code
+
+        private bool receive = true;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            outputLabel.Text = "Avviamento sistema...";
+
+            workThread = new BackgroundWorker();
+            workThread.DoWork += new DoWorkEventHandler(serverWork);
+
+            string[] lines = File.ReadAllLines(Path.Combine(Environment.CurrentDirectory, "settings.ini"));
+            dictionary = new Dictionary<string, string>();
+            commands = new Dictionary<string, string>();
+
+            outputBox.AppendText(format(OutLevel.INFO, "File 'settings.ini' caricato\n"));
+
+            bool apply = false;
+            foreach (var str in lines) {
+                str.Trim();
+
+                if(str.StartsWith(";#"))
+                {
+                    apply = true;
+                }
+
+                if(str.StartsWith(";") || str.Equals(""))
+                {
+                    continue;
+                }
+
+                string[] property = str.Split('=');
+
+                if(property.Length != 2)
+                {
+                    outputBox.AppendText(format(OutLevel.SEVERE, string.Format("La proprietà '{0}' non puo' essere caricata.\n", str)));
+                    continue;
+                }
+
+                if(apply)
+                {
+                    commands.Add(property[0], property[1]);
+                    outputBox.AppendText(format(OutLevel.INFO, string.Format("Comando '{0}' caricato con valore '{1}'\n", property[0], property[1])));
+                } else
+                {
+                    dictionary.Add(property[0], property[1]);
+                    outputBox.AppendText(format(OutLevel.INFO, string.Format("Proprietà '{0}' caricata con valore '{1}'\n", property[0], property[1])));
+                }
+            }
+
+            workThread.RunWorkerAsync();
+        }
+
+
+        private void serverWork(object sender, DoWorkEventArgs e)
+        {
+            string propertyPort = dictionary["port"];
+
+            port = new SerialPort()
+            {
+                PortName = propertyPort,
+                ReceivedBytesThreshold = int.Parse(dictionary["bytes-threshold"]),
+                Parity = Parity.None,
+                StopBits = StopBits.One,
+                BaudRate = int.Parse(dictionary["baud-rate"]),
+                DtrEnable = false,
+                RtsEnable = false,
+                DataBits = 8
+            };
+            port.DataReceived += new SerialDataReceivedEventHandler(dataReceive);
+
+            try
+            {
+                port.Open();
+            }
+            catch(Exception exception)
+            {
+                appendText("[Errore]:" + exception.Message);
+                return;
+            }
+
+            setText("Sistema avviato, in ascolto...", outputLabel);
+            appendText(format(OutLevel.INFO, string.Format("[Thread/{0}] Server avviato ed in ascolto sulla porta '{1}'\n", Thread.CurrentThread.ManagedThreadId, propertyPort)));
+        }
+
+        private char[] delims = new[] { '\r', '\n' };
+        private void dataReceive(object sender, SerialDataReceivedEventArgs e)
+        {
+            if (!receive) return;
+
+            SerialPort port = (SerialPort) sender;
+            while (port.BytesToRead > 0)
+            {
+                byte[] buffer = new byte[port.BytesToRead];
+                int count = port.Read(buffer, 0, port.BytesToRead);
+                string cmdr = "";
+
+                for(int i = 0; i < count; i++)
+                {
+                    cmdr += Convert.ToChar(buffer[i]);
+                }
+
+                string[] a = cmdr.Split(delims, StringSplitOptions.RemoveEmptyEntries);
+                foreach(var k in a)
+                {
+                    appendText(format(OutLevel.INFO, "Comando ricevuto: " + k + "\n"));
+                    k.Trim();
+                    if (k == null || k == "") return;
+
+                    performAction(k);
+                }
+            }
+        }
+
+        private void performAction(string command)
+        {
+            if(command.Length == 0)
+            {
+                return;
+            }
+
+            command.Trim();
+
+            switch(command)
+            {
+                default: break;
+                case "X1":
+                    pressKey(commands["X1"], 1);
+                    return;
+                case "X10":
+                    pressKey(commands["X10"], 1);
+                    return;
+                case "X100":
+                    pressKey(commands["X100"], 1);
+                    return;
+            }
+
+            string subcmd = command.Substring(0, 2);
+            int amount = 0;
+            
+            try
+            {
+                amount = int.Parse(command.Substring(2));
+            }
+            catch(FormatException e)
+            {
+                appendText("[Errore]: " + e.Message);
+            }
+
+            if(!commands.ContainsKey(subcmd))
+            {
+                return;
+            }
+
+            pressKey(commands[subcmd], amount);
+        }
+
+        private void pressKey(string key, int amount)
+        {
+            var processes = Process.GetProcessesByName(dictionary["process-name"]);
+            if (processes.Length == 0)
+            {
+                appendText("No Process found");
+                return;
+            }
+
+            AutomationElement element = AutomationElement.FromHandle(processes[0].MainWindowHandle);
+            if (element == null) return;
+            element.SetFocus();
+
+            byte value = byte.Parse(key, System.Globalization.NumberStyles.HexNumber);
+
+            for(int i = 0; i < amount; i++)
+            {
+                keybd_event(value, 0, KEYEVENTF_EXTENDEDKEY, 0);
+                keybd_event(value, 0, KEYEVENTF_KEYUP, 0);
+
+                string mout = i + ", " + key + " , " + value.ToString() + " , " + (value >> 8).ToString();
+                setText(mout, label3);
+            }
+        }
+
+        private string format(OutLevel level, string s) {
+            string result = "";
+
+            switch(level)
+            {
+                default:
+                case OutLevel.INFO:
+                    result += "[INFO]: ";
+                    break;
+                case OutLevel.WARNING:
+                    result += "[WARN]: ";
+                    break;
+                case OutLevel.SEVERE:
+                    result += "[SEVERE]: ";
+                    break;
+            }
+
+            result += s;
+
+            return result;
+        }
+
+        private enum OutLevel { INFO, WARNING, SEVERE }
+
+        delegate void appendTextCallback(string text);
+        delegate void setTextCallback(string text, Label label);
+
+        private void appendText(string text)
+        {
+            if(outputBox.InvokeRequired)
+            {
+                appendTextCallback callback = new appendTextCallback(appendText);
+                Invoke(callback, new object[] { text });
+            } else
+            {
+                outputBox.AppendText(text);
+            }
+        }
+
+        public void setText(string text, Label label)
+        {
+            if (outputLabel.InvokeRequired)
+            {
+                setTextCallback callback = new setTextCallback(setText);
+                Invoke(callback, new object[] { text, label });
+            }
+            else
+            {
+                label.Text = text;
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            port.Close();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            outputBox.Clear();
+        }
+
+        private void OutputBox_TextChanged(object sender, EventArgs e)
+        {
+            if(checkBox1.Checked)
+            {
+                outputBox.SelectionStart = outputBox.Text.Length;
+                outputBox.ScrollToCaret();
+            }
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            receive = !receive;
+
+            if(receive)
+            {
+                button2.Text = "Ferma la ricezione dati";
+                outputBox.AppendText(format(OutLevel.INFO, "Ricezione dati avviata\n"));
+                port.Open();
+            } else
+            {
+                button2.Text = "Abilita la ricezione dati";
+                outputBox.AppendText(format(OutLevel.INFO, "Ricezione dati fermata\n"));
+                port.Close();
+            }
+        }
+    }
+}
